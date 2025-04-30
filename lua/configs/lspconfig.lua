51d99501763c843a2bbaf848700abfb1d30a4fec
tada <ducvuongpham2004@outlook.com>
2025-04-30 10:50:03 +0900

    feat: add new lsp servers

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,6 +1,37 @@
 require("nvchad.configs.lspconfig").defaults()
 
-local servers = { "html", "cssls", "ts_ls", "gopls" }
+local servers = {
+  -- Web development
+  "html",
+  "cssls",
+  "ts_ls",
+  "angularls",
+  "cssmodules_ls",
+
+  -- Backend development
+  "gopls",
+  "pyright",
+  "sqls",
+  "java_language_server",
+
+  -- DevOps and Infrastructure
+  "ansiblels",
+  "dockerls",
+  "docker_compose_language_service",
+  "terraformls",
+
+  -- Configuration and Scripting
+  "nginx_language_server",
+  "nixd",
+  "lua_ls",
+  "jsonls",
+  "bashls",
+  "yamlls",
+
+  -- C/C++ development
+  "cmake",
+  "clangd",
+}
 vim.lsp.enable(servers)
 -- local nvlsp = require "nvchad.configs.lspconfig"
 -- local lspconfig = require "lspconfig"
--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -1,29 +1,29 @@
 -- Editor enhancement plugins
 return {
   {
-    'nvim-telescope/telescope-fzf-native.nvim',
-    build = 'make'
+    "nvim-telescope/telescope-fzf-native.nvim",
+    build = "make",
   },
 
   {
-    'nvim-treesitter/nvim-treesitter-context',
+    "nvim-treesitter/nvim-treesitter-context",
     lazy = false,
     config = function()
-      require('treesitter-context').setup {
-        enable = true,            -- Enable this plugin (Can be enabled/disabled later via commands)
-        max_lines = 0,            -- How many lines the window should span. Values <= 0 mean no limit.
-        min_window_height = 0,    -- Minimum editor window height to enable context. Values <= 0 mean no limit.
+      require("treesitter-context").setup {
+        enable = true, -- Enable this plugin (Can be enabled/disabled later via commands)
+        max_lines = 0, -- How many lines the window should span. Values <= 0 mean no limit.
+        min_window_height = 0, -- Minimum editor window height to enable context. Values <= 0 mean no limit.
         line_numbers = true,
         multiline_threshold = 20, -- Maximum number of lines to show for a single context
-        trim_scope = 'outer',     -- Which context lines to discard if `max_lines` is exceeded. Choices: 'inner', 'outer'
-        mode = 'cursor',          -- Line used to calculate context. Choices: 'cursor', 'topline'
+        trim_scope = "outer", -- Which context lines to discard if `max_lines` is exceeded. Choices: 'inner', 'outer'
+        mode = "cursor", -- Line used to calculate context. Choices: 'cursor', 'topline'
         -- Separator between context and content. Should be a single character string, like '-'.
         -- When separator is set, the context will only show up when there are at least 2 lines above cursorline.
         separator = nil,
-        zindex = 20,     -- The Z-index of the context window
+        zindex = 20, -- The Z-index of the context window
         on_attach = nil, -- (fun(buf: integer): boolean) return false to disable attaching
       }
-    end
+    end,
   },
 
   {
@@ -52,14 +52,14 @@ return {
           },
         },
       }
-    end
+    end,
   },
-  
+
   {
     "stevearc/conform.nvim",
     -- event = 'BufWritePre', -- uncomment for format on save
-    opts = function() 
-      if vim.fn.filereadable(vim.fn.stdpath("config") .. "/lua/configs/conform.lua") == 1 then
+    opts = function()
+      if vim.fn.filereadable(vim.fn.stdpath "config" .. "/lua/configs/conform.lua") == 1 then
         return require "configs.conform"
       end
     end,
@@ -69,28 +69,32 @@ return {
     "nvim-treesitter/nvim-treesitter",
     opts = {
       ensure_installed = {
-        "vim", "lua", "vimdoc",
-        "html", "css"
+        "vim",
+        "lua",
+        "vimdoc",
+        "html",
+        "css",
       },
     },
   },
-  
+
   {
     "MeanderingProgrammer/render-markdown.nvim",
     opts = {
       file_types = {
         "markdown",
-        "Avante" -- Add Avante filetype for markdown rendering
-      }
+        "Avante", -- Add Avante filetype for markdown rendering
+      },
     },
     ft = {
       "markdown",
-      "Avante"
+      "Avante",
     },
   },
-  
+
   {
-    'stevearc/dressing.nvim',
-    event = 'VeryLazy'
+    "stevearc/dressing.nvim",
+    event = "VeryLazy",
   },
 }
+
